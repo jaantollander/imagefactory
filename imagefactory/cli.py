ad9bdebe96e45999f0fbc6dd613468f632729877
Jaan <jaan.tollander@gmail.com>
2017-01-12 13:18:04 +0200

    Added help texts

--- imagefactory/cli.py
+++ imagefactory/cli.py
@@ -11,12 +11,19 @@ from imagefactory.imagefactory import create_image
 
 @click.command()
 @click.version_option()
-@click.option('--name', '-n', default='untitled')
-@click.option('--width', '-w', default=48)
-@click.option('--height', '-h', default=48)
-@click.option('--filetype', '-ft', default='png')
-@click.option('--text', '-t', default=None)
-@click.option('--savedir', '-d', default=None)
+@click.option('--name', '-n', default='untitled',
+              help='Name of the file. If name is written with extension it'
+                   'overwrites any filetype that is supplied.')
+@click.option('--width', '-w', default=48,
+              help='Width of the image')
+@click.option('--height', '-h', default=48,
+              help='Height of the image.')
+@click.option('--filetype', '-ft', default='png',
+              help='Filetype of the image. For example "png" or "svg"')
+@click.option('--text', '-t', default=None,
+              help='Text that is written in the image.')
+@click.option('--savedir', '-d', default='',
+              help='Directory where image should be saved.')
 def main(name, width, height, filetype, text, savedir):
     """Console script for imagefactory"""
     create_image(name, width, height, filetype, text, savedir)
